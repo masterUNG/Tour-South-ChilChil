3854975b07c92acbc3f67d4207169e1cca232233
masterUNG <phrombutr@gmail.com>
2016-03-24 15:43:43 +0700

    Delete All fihish

--- app/src/main/java/appewtc/masterung/toursouthchilchil/MainActivity.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/MainActivity.java
@@ -1,6 +1,7 @@
 package appewtc.masterung.toursouthchilchil;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //Test Add Value
-        testAddValue();
+        //testAddValue();
+
+        //Delete All data
+        deleteAllData();
 
     }   // Main Method
+
+    private void deleteAllData() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+        sqLiteDatabase.delete(MyManage.tour_table, null, null);
+    }
 
     private void testAddValue() {
         myManage.addUser("user", "pass", "name", "email");

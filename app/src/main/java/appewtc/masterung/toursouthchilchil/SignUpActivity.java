0cbde03da8621027d371faedbbf84e2299ccf71b
masterUNG <phrombutr@gmail.com>
2016-03-24 14:05:27 +0700

    SignUp Successful

--- app/src/main/java/appewtc/masterung/toursouthchilchil/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/SignUpActivity.java
@@ -1,9 +1,20 @@
 package appewtc.masterung.toursouthchilchil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -40,10 +51,45 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            updateToServer();
 
         }
 
     }   // clickSign
+
+    private void updateToServer() {
+
+        //Connected http
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("User", userString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+            nameValuePairs.add(new BasicNameValuePair("Name", nameString));
+            nameValuePairs.add(new BasicNameValuePair("Email", emailString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/saa/php_add_user_master.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(SignUpActivity.this, "บันทึกข้อมูล เรียบร้อยแล้ว ขอบคุณคะ",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+
+        } catch (Exception e) {
+            MyAlertDialog myAlertDialog = new MyAlertDialog();
+            myAlertDialog.myDialog(SignUpActivity.this, "Error", "ไม่สามารถเชื่อต่อ Server ได้");
+        }
+
+
+
+    }   // updateToServer
 
     private boolean checkSpace() {
 

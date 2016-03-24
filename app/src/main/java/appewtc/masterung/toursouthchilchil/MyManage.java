8325a59b3c8bcb4603b0e0ddfe84d9a288f65afa
masterUNG <phrombutr@gmail.com>
2016-03-24 15:09:52 +0700

    ประกาศตัวแปร แล้ว

--- app/src/main/java/appewtc/masterung/toursouthchilchil/MyManage.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/MyManage.java
@@ -12,6 +12,24 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    private static final String column_id = "_id";
+    private static final String column_User = "User";
+    private static final String column_Password = "Password";
+    private static final String column_Name = "Name";
+    private static final String column_Email = "Email";
+
+
+    public static final String tour_table = "tourTABLE";
+    private static final String column_Province = "Province";
+    private static final String column_District = "District";
+    private static final String column_Category = "Category";
+    private static final String column_Description = "Description";
+    private static final String column_Image = "Image";
+    private static final String column_Lat = "Lat";
+    private static final String column_Lng = "Lng";
+    private static final String column_Range = "Range";
+
 
     public MyManage(Context context) {
 

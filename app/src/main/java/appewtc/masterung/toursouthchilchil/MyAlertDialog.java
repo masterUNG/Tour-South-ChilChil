4cba31d7e33b7a844c024541c63eddede69eafc5
masterUNG <phrombutr@gmail.com>
2016-03-24 11:50:05 +0700

    Check Space Finish

---
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/MyAlertDialog.java
@@ -0,0 +1,30 @@
+package appewtc.masterung.toursouthchilchil;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 3/24/16 AD.
+ */
+public class MyAlertDialog {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setIcon(R.drawable.icon_question);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/toursouthchilchil/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/SignUpActivity.java
@@ -2,12 +2,65 @@ package appewtc.masterung.toursouthchilchil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, emailEditText,
+            userEditText, passwordEditText;
+    private String nameString, emailString, userString,
+            passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        emailString = emailEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (checkSpace()) {
+            //Have Space
+            MyAlertDialog myAlertDialog = new MyAlertDialog();
+            myAlertDialog.myDialog(SignUpActivity.this, "มีช่องว่าง",
+                    "กรุณากรอก ทุกช่องคะ");
+
+        } else {
+            //No Space
+
+        }
+
+    }   // clickSign
+
+    private boolean checkSpace() {
+
+        boolean bolResult = true;
+
+        bolResult = nameString.equals("") || emailString.equals("") ||
+                userString.equals("") || passwordString.equals("");
+
+        return bolResult;
     }
-}
+
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        emailEditText = (EditText) findViewById(R.id.editText4);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+    }
+
+}   // Main Class

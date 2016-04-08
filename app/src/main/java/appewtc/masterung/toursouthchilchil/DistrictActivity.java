c3681c20c99e2fd994e330f4b3cd626b120aa066
masterUNG <phrombutr@gmail.com>
2016-04-08 11:02:11 +0700

    Show District Finish

--- app/src/main/java/appewtc/masterung/toursouthchilchil/DistrictActivity.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/DistrictActivity.java
@@ -1,7 +1,11 @@
 package appewtc.masterung.toursouthchilchil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DistrictActivity extends AppCompatActivity {
@@ -12,13 +16,34 @@ public class DistrictActivity extends AppCompatActivity {
         setContentView(R.layout.activity_district);
 
         //Receive from Intent
-        String[] districtStrings = getIntent().getStringArrayExtra("District");
+        final String[] districtStrings = getIntent().getStringArrayExtra("District");
         String strProvince = getIntent().getStringExtra("Province");
 
         //Show Province
         TextView provinceTextView = (TextView) findViewById(R.id.textView8);
         provinceTextView.setText("อำเภอใน จังหวัด " + strProvince);
 
+        //Show ListView
+        MyAdapter myAdapter = new MyAdapter(this, districtStrings);
+        ListView listView = (ListView) findViewById(R.id.listView2);
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(DistrictActivity.this, TourActivity.class);
+                intent.putExtra("District", districtStrings[i]);
+                startActivity(intent);
+
+            }   // onItem
+        });
 
     }   // Main Method
+
+    public void clickBackDistrict(View view) {
+        finish();
+    }
+
+
 }   // Main Class
--- app/src/main/java/appewtc/masterung/toursouthchilchil/TourActivity.java
+++ app/src/main/java/appewtc/masterung/toursouthchilchil/TourActivity.java
@@ -2,12 +2,35 @@ package appewtc.masterung.toursouthchilchil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TourActivity extends AppCompatActivity {
+
+    //Explicit
+    private String districtString;
+    private TextView districtTextView;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+
+        //Bind Widget
+        districtTextView = (TextView) findViewById(R.id.textView9);
+
+        //Receive Value
+        districtString = getIntent().getStringExtra("District");
+        districtTextView.setText(districtString);
+
+
+
+    }   // Main Method
+
+    public void clickBackTour(View view) {
+        finish();
     }
-}
+
+}   // Main Class
